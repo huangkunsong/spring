8ecbddbe7c9c7b71cccc46c2dcf5d4e0621c1db0
huangkunsong <huang_kunsong@sina.cn>
2018-02-07 21:44:21 +0800

    +++++++++++++++

--- src/main/java/com/hks/websocket/Stomp.java
+++ src/main/java/com/hks/websocket/Stomp.java
@@ -4,9 +4,11 @@ import com.hks.entity.StompMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
@@ -15,6 +17,9 @@ import java.security.Principal;
 public class Stomp {
     private static final Logger LOGGER = LoggerFactory.getLogger(Stomp.class);
 
+    /**
+     * 可以在任意地方注入,推送消息
+     */
     @Autowired
     private SimpMessageSendingOperations messaging;
     /**
@@ -34,5 +39,18 @@ public class Stomp {
     @MessageMapping("/hi")
     public void handleHi(Principal principal, StompMessage stomp) {
         messaging.convertAndSend("/topic/hi", "hi,Java");
+    }
+
+    /**
+     * MessageExceptionHandler，WebSocket报错时触发
+     * 记录和推送错误信息
+     * @param e
+     * @return
+     */
+    @MessageExceptionHandler
+    @SendToUser("/queue/errors")
+    public String handleException(Throwable e){
+        LOGGER.error(e.getMessage(), e);
+        return e.getMessage();
     }
 }
